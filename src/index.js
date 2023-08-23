23f8fa2f77ea5afaa6d8743a22feabbc45929974
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2023-08-23 13:30:38 +0700

    chore: add eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import dotenv from 'dotenv'
-import cors from 'cors'
-import express from 'express'
+import dotenv from 'dotenv';
+import cors from 'cors';
+import express from 'express';
 
 dotenv.config();
 const PORT = process.env.PORT || 2000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 
 app.listen(PORT, () => console.log(`Server started on port:${PORT}`));
