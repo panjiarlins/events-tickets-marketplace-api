b2c695f89b6c957942443c0ab1617276babe5271
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2023-08-29 12:06:10 +0700

    feat: update controller for user and event

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -1,4 +1,6 @@
-const { Event } = require('../models');
+const {
+  Sequelize, sequelize, User, Event,
+} = require('../models');
 
 const eventController = {
   getAllEvents: async (req, res) => {
@@ -28,17 +30,7 @@ const eventController = {
       }
       res.status(200).json({
         status: 'success',
-        data: {
-          id: result.id,
-          title: result.title,
-          imageUrl: result.imageUrl,
-          city: result.city,
-          address: result.address,
-          description: result.description,
-          price: result.price,
-          stock: result.stock,
-          startAt: result.startAt,
-        },
+        data: result,
       });
     } catch (error) {
       res.status(500).json({
@@ -48,35 +40,73 @@ const eventController = {
     }
   },
 
-  createEvent(req, res) {
-    Event.create({ ...req.body })
-      .then((result) => res.send(result))
-      .catch((err) => res.status(500).send(err?.message));
+  createEvent: async (req, res) => {
+    try {
+      const userData = await User.findOne({
+        where: { id: req.body.userId },
+      });
+      if (!userData) {
+        res.status(400).json({
+          status: 'error',
+          message: 'userId not exist',
+        });
+        return;
+      }
+      const result = await Event.create(
+        req.body,
+      );
+      res.status(201).json({
+        status: 'success',
+        data: result,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error,
+      });
+    }
   },
 
-  editEvent(req, res, next) {
-    const { id } = req.params;
-    Event.update({
-      ...req.body,
-    }, {
-      where: { id },
-    }).then(() => next())
-      .catch((err) => res.status(500).send(err?.message));
+  editEvent: async (req, res, next) => {
+    try {
+      await sequelize.transaction({
+        isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.SERIALIZABLE,
+      }, async (t) => {
+        const { id } = req.params;
+        await Event.update(
+          req.body,
+          {
+            where: { id },
+            fields: [
+              'title', 'imageUrl', 'city', 'address',
+              'description', 'price', 'stock', 'startAt',
+            ],
+            transaction: t,
+          },
+        );
+      });
+      next();
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error,
+      });
+    }
   },
 
   deleteEventById: async (req, res) => {
     try {
       const result = await Event.destroy({
-        where: {
-          id: req.params.id,
-        },
+        where: { id: req.params.id },
       });
       if (!result) {
         res.status(404).json({
           status: 'error',
           message: 'event not found',
-        }); return;
-      } res.send('deleted success');
+        });
+        return;
+      }
+      res.sendStatus(204);
     } catch (error) {
       res.status(500).json({
         status: 'error',
