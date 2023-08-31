2a2df3d32dfda341763bc6dc11d80e4fdd657982
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2023-08-31 07:56:41 +0700

    feat: create getOrderById

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -20,6 +20,33 @@ const orderController = {
       });
     }
   },
+
+  getOrderById: async (req, res) => {
+    try {
+      const result = await Order.findByPk(req.params.id, {
+        include: [
+          { model: Event },
+          { model: Review },
+        ],
+      });
+      if (!result) {
+        res.status(404).json({
+          status: 'error',
+          message: 'order not found',
+        });
+        return;
+      }
+      res.status(200).json({
+        status: 'success',
+        data: result,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error,
+      });
+    }
+  },
 };
 
 module.exports = orderController;
--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const { orderController } = require('../controllers');
 
 router.get('/', orderController.getAllOrders);
+router.get('/:id', orderController.getOrderById);
 
 module.exports = router;
