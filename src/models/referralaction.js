bcdade276fef31d38935d2ecf1ee89012d91c019
Moch Ridwan Qodri <ridwanqodryy@gmail.com>
2023-09-03 18:15:20 +0700

    feat: add models referralaction

---
+++ src/models/referralaction.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const { Model, INTEGER } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class ReferralAction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      //   define association here
+      models.ReferralAction.belongsTo(models.User, {
+        foreignKey: {
+          name: 'referrerUserId',
+          primaryKey: true,
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      });
+      models.ReferralAction.belongsTo(models.User, {
+        foreignKey: {
+          name: 'referredUserId',
+          primaryKey: true,
+          unique: true,
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      });
+    }
+  }
+
+  ReferralAction.init(
+    {},
+    {
+      sequelize,
+      modelName: 'ReferralAction',
+    },
+  );
+
+  ReferralAction.removeAttribute('id');
+
+  return ReferralAction;
+};
