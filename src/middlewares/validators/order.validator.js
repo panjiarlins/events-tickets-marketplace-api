a2fdb29d8546221622020e2ea03b0744ffd57bfd
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2023-09-03 15:49:44 +0700

    feat: add order validator

---
+++ src/middlewares/validators/order.validator.js
@@ -0,0 +1,82 @@
+const Joi = require('joi');
+const { ResponseError } = require('../../errors');
+
+const orderValidator = {
+  getOrderById: (req, res, next) => {
+    try {
+      const schema = Joi.object({
+        id: Joi.number().integer().min(1).required(),
+      }).required();
+
+      const result = schema.validate(req.params);
+      if (result.error) throw new ResponseError(result.error.message, 400);
+
+      next();
+    } catch (error) {
+      res.status(error.statusCode || 500).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  },
+
+  createOrder: (req, res, next) => {
+    try {
+      const schema = Joi.object({
+        userId: Joi.string().guid().required(),
+        eventId: Joi.number().integer().min(1).required(),
+        quantity: Joi.number().integer().min(1).required(),
+        voucherCode: Joi.string().optional(),
+      }).required();
+
+      const result = schema.validate(req.body);
+      if (result.error) throw new ResponseError(result.error.message, 400);
+
+      next();
+    } catch (error) {
+      res.status(error.statusCode || 500).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  },
+
+  payOrder: (req, res, next) => {
+    try {
+      const schema = Joi.object({
+        id: Joi.number().integer().min(1).required(),
+      }).required();
+
+      const result = schema.validate(req.body);
+      if (result.error) throw new ResponseError(result.error.message, 400);
+
+      next();
+    } catch (error) {
+      res.status(error.statusCode || 500).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  },
+
+  sendPaymentEmail: (req, res, next) => {
+    try {
+      const schema = Joi.object({
+        id: Joi.number().integer().min(1).required(),
+      }).required();
+
+      const result = schema.validate(req.body);
+      console.log(result);
+      if (result.error) throw new ResponseError(result.error.message, 400);
+
+      next();
+    } catch (error) {
+      res.status(error.statusCode || 500).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  },
+};
+
+module.exports = orderValidator;
