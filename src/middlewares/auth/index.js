2fef21607cf87728c7172ca5b0269d76eee4d935
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2023-09-09 15:06:22 +0700

    feat: add orderAuth

--- src/middlewares/auth/index.js
+++ src/middlewares/auth/index.js
@@ -2,5 +2,6 @@ module.exports = {
   userAuth: require('./user.auth'),
   eventAuth: require('./event.auth'),
   voucherAuth: require('./voucher.auth'),
+  orderAuth: require('./order.auth'),
   reviewAuth: require('./review.auth'),
 };
